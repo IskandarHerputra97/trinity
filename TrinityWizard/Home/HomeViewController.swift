e50d66b0e8d70273e54f1026b0304a6134792d5b
Iskandar Herputra Wahidiyat <iskandarherputra@gmail.com>
2023-07-03 12:53:08 +0700

    Basic setup

--- TrinityWizard/Home/HomeViewController.swift
+++ TrinityWizard/Home/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  TrinityWizard
 //
 //  Created by Iskandar Herputra Wahidiyat on 03/07/23.
@@ -7,13 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
-
